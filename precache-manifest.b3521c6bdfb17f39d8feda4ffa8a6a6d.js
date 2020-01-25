466fd5a2f891534e034843ebe62a3a8f21c9aa6d
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-01-25 22:23:05 +0200

    Updates

---
+++ precache-manifest.b3521c6bdfb17f39d8feda4ffa8a6a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8198f61bff74e73f61fbbc2b88c15668",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "af9cc73b1f7f990d0fa5",
+    "url": "/blog-react/static/css/main.dbc2ee6b.chunk.css"
+  },
+  {
+    "revision": "1bafb7c9c1eb2fb7a05f",
+    "url": "/blog-react/static/js/2.4eb4feca.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/blog-react/static/js/2.4eb4feca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af9cc73b1f7f990d0fa5",
+    "url": "/blog-react/static/js/main.079e5f5c.chunk.js"
+  },
+  {
+    "revision": "174d566a1a3d9a58c3a1",
+    "url": "/blog-react/static/js/runtime-main.56247c33.js"
+  }
+]);
